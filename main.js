05ba8bcd0d3d90a126616cfe9c502759266b1e9f
zivl <levi.ziv@gmail.com>
2017-01-05 22:15:12 +0200

    add zoom functionality

--- main.js
+++ main.js
@@ -12,18 +12,24 @@ function loadJSON(callback) {
 	xobj.send(null);
 }
 
+function zoom() {
+	let {transform} = d3.event;
+	rootGroup.attr('transform', 'translate(' + transform.x + ',' + transform.y + ') scale(' + transform.k + ')');
+}
+
+
 const radius = 30,
 	svg = d3.select('svg'),
-	width = Number(svg.style('width').replace('px','')),
-	height = Number(svg.style('height').replace('px',''));
-
+	rootGroup = svg.append('g').attr('class', 'zoom-wrapper').call(d3.zoom().on('zoom', zoom)),
+	width = Number(svg.style('width').replace('px', '')),
+	height = Number(svg.style('height').replace('px', ''));
 
 let simulation = d3.forceSimulation()
-	.force('link', d3.forceLink().id(function (d) { return d.id; }))
+	.force('link', d3.forceLink().id(d => d.id))
 	.force('charge', d3.forceCollide(radius * 3))
 	.force('center', d3.forceCenter(width / 2, height / 2));
 
-function buildGraph(movies) {
+function buildGraphData(movies) {
 	let nodes = [];
 	let links = [];
 	movies.forEach(movie => {
@@ -40,41 +46,35 @@ function buildGraph(movies) {
 
 loadJSON(function (data) {
 
-	let graph = buildGraph(data);
-	let link = svg.append('g')
+	let graph = buildGraphData(data);
+	let link = rootGroup.append('g')
 		.attr('class', 'links')
 		.selectAll('line')
 		.data(graph.links)
 		.enter().append('line')
 		.attr('stroke-width', 1);
 
-	let node = svg.append('g')
+	let node = rootGroup.append('g')
 		.attr('class', 'nodes')
 		.selectAll('circle')
 		.data(graph.nodes)
 		.enter().append('circle')
 		.attr('r', radius)
-		.attr('fill', function (d) { return `url(#${d.image})`; })
-		.call(d3.drag()
-			.on('start', dragstarted)
-			.on('drag', dragged)
-			.on('end', dragended));
+		.attr('fill', d => `url(#${d.image})`)
+		.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
 
-	node.append('title').text(function (d) { return d.id; });
+	node.append('title').text(d => d.id);
 
 	simulation.nodes(graph.nodes).on('tick', ticked);
 
 	simulation.force('link').links(graph.links);
 
 	function ticked() {
-		link
-			.attr('x1', function (d) { return d.source.x; })
-			.attr('y1', function (d) { return d.source.y; })
-			.attr('x2', function (d) { return d.target.x; })
-			.attr('y2', function (d) { return d.target.y; });
+		link.attr('x1', d => d.source.x).attr('y1', d => d.source.y)
+			.attr('x2', d => d.target.x).attr('y2', d => d.target.y);
 
-		node.attr("cx", function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
-			.attr("cy", function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
+		node.attr('cx', d => d.x = Math.max(radius, Math.min(width - radius, d.x)))
+			.attr('cy', d => d.y = Math.max(radius, Math.min(height - radius, d.y)));
 	}
 });
 
